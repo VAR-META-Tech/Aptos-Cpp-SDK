2eb651807e5c373966880a08e2032f8f0ec09d29
Dev BRX/ARX <nguyenducanhkhoa96@gmail.com>
2023-11-21 21:34:03 +0700

    Add AptosUI to branch AptosUI
    Add AptosUI to branch AptosUI

---
+++ AptopsUI/Source/AptopsDemo/AptopsDemo.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AptopsDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AptopsDemo, "AptopsDemo" );
