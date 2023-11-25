0c478ad497c549c377ee6c771fb132118a4078db
Dev BRX/ARX <nguyenducanhkhoa96@gmail.com>
2023-11-25 23:15:15 +0700

    Update UI For Notification
    Update UI For Notification

--- AptosUI/Source/AptosUI/AptosUI.Build.cs
+++ AptosUI/Source/AptosUI/AptosUI.Build.cs
@@ -7,14 +7,14 @@ public class AptosUI : ModuleRules
 	public AptosUI(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		PrivateDependencyModuleNames.AddRange(new string[] { });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
