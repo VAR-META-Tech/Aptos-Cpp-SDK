80d0ffaea607de44935259a7ad32d86a8762f5b2
Dev BRX/ARX <nguyenducanhkhoa96@gmail.com>
2023-11-28 00:06:16 +0700

    Update UIController
    Update UIController

---
+++ AptosUI/Source/AptosUI/UIController.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "UIController.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class APTOSUI_API UUIController : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "OnCreateWallet Clicked"), Category = "UIController")
+	static void OnCreateWalletClicked();
+};
