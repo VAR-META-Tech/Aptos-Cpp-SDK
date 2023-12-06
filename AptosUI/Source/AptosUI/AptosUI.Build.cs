2e7a9a301ba57cc5a8e78b16856ea3871011917a
Dev BRX/ARX <nguyenducanhkhoa96@gmail.com>
2023-12-06 16:56:19 +0700

    Update Build Mac

--- AptosUI/Source/AptosUI/AptosUI.Build.cs
+++ AptosUI/Source/AptosUI/AptosUI.Build.cs
@@ -12,17 +12,18 @@ public class AptosUI : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" });
 		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		PrivateDependencyModuleNames.AddRange(new string[] { });
-		string AptosUiLogicPath = Path.Combine(ModuleDirectory, "../../../build/", "libAptosUILogic.dylib");
+		string AptosUiLogicPath = Path.Combine(ModuleDirectory, "../../../build/", "libAptosUILogic.a");
 		string AptosLibPath = Path.Combine(ModuleDirectory, "../../../build/", "libAptos.dylib");
 		string destinationDirectory = Target.ProjectFile.Directory.FullName;
-		File.Copy(AptosUiLogicPath, Path.Combine(destinationDirectory, "libAptosUILogic.dylib"), true);
+		File.Copy(AptosUiLogicPath, Path.Combine(destinationDirectory, "libAptosUILogic.a"), true);
 		File.Copy(AptosLibPath, Path.Combine(destinationDirectory, "libAptos.dylib"), true);
 
 		PublicIncludePaths.AddRange(new string[] { Path.Combine(ModuleDirectory, "../../../") });
-		PublicAdditionalLibraries.Add(Path.Combine(destinationDirectory, "libAptosUILogic.dylib"));
+		PublicAdditionalLibraries.Add(Path.Combine(destinationDirectory, "libAptosUILogic.a"));
 		PublicAdditionalLibraries.Add(Path.Combine(destinationDirectory, "libAptos.dylib"));
-		//PublicAdditionalLibraries.Add("/usr/local/lib/libbitcoin-system.so");
-		//PublicAdditionalLibraries.Add("/usr/local/lib/libcpprest.so");
+		PublicAdditionalLibraries.Add("/opt/homebrew/lib/libbitcoin-system.0.dylib");
+		PublicAdditionalLibraries.Add("/opt/homebrew/lib/libcpprest.2.10.dylib");
+		PublicAdditionalLibraries.Add("/opt/homebrew/lib/libcrypto.3.dylib");
 
 		PublicIncludePaths.AddRange(new string[] { "/usr/local/include/" });
 		//bUseRTTI = true;
