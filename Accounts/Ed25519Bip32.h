cd9d532dfe001cdfb9d2cddfa95ccd4559104ee0
AnhVAR <anhnguyen@var-meta.com>
2024-01-03 13:40:27 +0700

    Fix bug mac M1

--- Accounts/Ed25519Bip32.h
+++ Accounts/Ed25519Bip32.h
@@ -1,24 +1,5 @@
 #ifndef ED25519BIP32_H
 #define ED25519BIP32_H
-#if defined(__APPLE__)
-namespace std
-{
-    template <class Arg, class Result>
-    struct unary_function
-    {
-        typedef Arg argument_type;
-        typedef Result result_type;
-    };
-
-    template <class Arg1, class Arg2, class Result>
-    struct binary_function
-    {
-        typedef Arg1 first_argument_type;
-        typedef Arg2 second_argument_type;
-        typedef Result result_type;
-    };
-}
-#endif
 #include <iostream>
 #include <sstream>
 #include <vector>
